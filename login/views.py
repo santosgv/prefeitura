5a2ca7a36d23459b21205b5991ad907850fad7be
carlosefl <art97496@gmail.com>
2023-07-20 00:13:27 -0300

    QUE PERTUBAÇÃO MEU VEI MAS CONSEGUI tela login ok

--- login/views.py
+++ login/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def gestor(request):
-    return render(request, 'login.html')
+    return render(request, 'html/login.html')
 
 
 def adm(request):
-    return render(request, 'login.html' )
+    return render(request, 'html/login.html' )
