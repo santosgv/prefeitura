57576bd412b064d66f50b8ae251985ca275fe620
carlosefl <art97496@gmail.com>
2023-07-25 23:17:55 -0300

    corrigi o banco

--- login/models.py
+++ login/models.py
@@ -5,7 +5,7 @@ class Usuario(models.Model):
     nome = models.CharField(max_length=30)
     matricula = models.CharField(max_length= 30)
     senha = models.CharField(max_length=64)
-    
+    local = models.CharField(max_length=65, default='N/A')
     
     def __str__(self) -> str:
         return self.nome
